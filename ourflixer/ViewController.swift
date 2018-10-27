9e845499f51ce40f8f5d1dfdf6a4cf10317311a0
Ethan Chen <ktun7@mail.ccsf.edu>
2018-10-26 19:16:08 -0700

    Initial Commit

---
+++ ourflixer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ourflixer
+//
+//  Created by Ethan Chen on 10/26/18.
+//  Copyright © 2018 kyaw tun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
